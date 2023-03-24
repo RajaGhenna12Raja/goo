efe307f312bb2355cca7a834ef438f68131f7f45
RajaGhenna12Raja <104801015+RajaGhenna12Raja@users.noreply.github.com>
2023-03-24 21:58:25 +0530

    added favicon
    Signed-off-by: RajaGhenna12Raja <104801015+RajaGhenna12Raja@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div className='text-red-600'>
+    <div className='text-red-600 text-3xl'>
       this is Home
     </div>
   )
